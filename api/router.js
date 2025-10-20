a19a2c5b245815376b9efb9b9cc3412b8a8e5392
Trading elite <tradingviewelite2@gmail.com>
2025-10-20 23:30:56 +0200

    Update router.js

--- api/router.js
+++ api/router.js
@@ -574,7 +574,7 @@ const catalogData = [
           episode: 54,
           overview: "Épisode 54 de la saison 10",
           released: "2025-10-24T00:00:00.000Z",
-          thumbnail: "https://photos.tf1.fr/330/186/avant-premiere-la-villa-saison-10-episode-54-du-22-octobre-2025-99473696-1760599682-7416a4-2ba6cf-0@3x.jpg",
+          thumbnail: "https://photos.tf1.fr/330/186/avant-premiere-la-villa-saison-10-episode-53-du-21-octobre-2025-54944996-1760517199-e852fb-d14ec9-0@3x.jpg",
           stream: "https://poetic-cendol-6fcb8a.netlify.app/master.m3u8"
         }
       ]
